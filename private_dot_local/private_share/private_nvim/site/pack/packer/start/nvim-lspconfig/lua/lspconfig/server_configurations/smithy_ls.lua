41332a8d7ff4ec69f392f931fa3849fa73aa38ef
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-10-09 22:35:23 +0100

    Add smithy LSP config

---
+++ private_dot_local/private_share/private_nvim/site/pack/packer/start/nvim-lspconfig/lua/lspconfig/server_configurations/smithy_ls.lua
@@ -0,0 +1,23 @@
+local util = require 'lspconfig.util'
+
+return {
+  default_config = {
+    cmd = { 'cs', 'launch', 'com.disneystreaming.smithy:smithy-language-server:0.0.28', '--ttl', '1h', '--repository', 'm2Local', '--main-class', 'software.amazon.smithy.lsp.Main', '--', '0' },
+    filetypes = { 'smithy' },
+    root_dir = util.root_pattern('smithy-build.json'),
+    message_level = vim.lsp.protocol.MessageType.Log,
+    init_options = {
+      statusBarProvider = 'show-message',
+      isHttpEnabled = true,
+      compilerOptions = {
+        snippetAutoIndent = false,
+      },
+    },
+  },
+  docs = {
+    description = [[ Wake and bake, slumming it bruh ]],
+    default_config = {
+      root_dir = [[util.root_pattern("smithy-build.json")]],
+    },
+  },
+}
