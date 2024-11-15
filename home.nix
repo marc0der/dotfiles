d608cda0a8fafaf2e5351687a84eb9b38e10b830
Marco Vermeulen <vermeulen.mp@gmail.com>
2024-11-15 14:53:55 +0000

    Manage modularised sway config

--- home.nix
+++ home.nix
@@ -26,13 +26,21 @@
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" "Noto" ]; })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
     ".config/sway/config".source = sway/config;
+    ".config/sway/config.d/50-rules-browser.conf".source = sway/config.d/50-rules-browser.conf;
+    ".config/sway/config.d/50-rules-pavucontrol.conf".source = sway/config.d/50-rules-pavucontrol.conf;
+    ".config/sway/config.d/50-rules-policykit-agent.conf".source = sway/config.d/50-rules-policykit-agent.conf;
+    ".config/sway/config.d/60-bindings-brightness.conf".source = sway/config.d/60-bindings-brightness.conf;
+    ".config/sway/config.d/60-bindings-media.conf".source = sway/config.d/60-bindings-screenshot.conf;
+    ".config/sway/config.d/60-bindings-screenshot.conf".source = sway/config.d/60-bindings-volume.conf;
+    ".config/sway/config.d/60-bindings-volume.conf".source = sway/config.d/65-mode-passthrough.conf;
+    ".config/sway/config.d/65-mode-passthrough.conf".source = sway/config.d/90-bar.conf;
+    ".config/sway/config.d/90-bar.conf".source = sway/config.d/90-swayidle.conf;
+    ".config/sway/config.d/90-swayidle.conf".source = sway/config.d/95-autostart-policykit-agent.conf;
+    ".config/sway/config.d/95-autostart-policykit-agent.conf".source = sway/config.d/95-xdg-desktop-autostart.conf;
+    ".config/sway/config.d/95-xdg-desktop-autostart.conf".source = sway/config.d/95-xdg-user-dirs.conf;
+    ".config/sway/config.d/95-xdg-user-dirs.conf".source = sway/config.d/95-xdg-user-dirs.conf;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
