8a5d91404f1d18098ed80a122db270fa882ba9a5
Marco Vermeulen <vermeulen.mp@gmail.com>
2024-11-15 14:40:08 +0000

    Manage sway config file

--- home.nix
+++ home.nix
@@ -32,7 +32,7 @@
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    "~/.config/sway/config".source = ./sway/config;
+    ".config/sway/config".source = sway/config;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
