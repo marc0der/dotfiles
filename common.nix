42f023a242c65d17c1a8399b75f59184941164c0
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-02-02 22:35:09 +0000

    Simplify common.nix

--- common.nix
+++ common.nix
@@ -2,8 +2,8 @@
 
 {
   home.username = "marco";
-  home.homeDirectory = "/var/home/marco";
-  home.stateVersion = "24.05";
+  home.homeDirectory = "/home/marco";
+  home.stateVersion = "24.11";
 
   nixpkgs.config.allowUnfree = true;
 
@@ -12,10 +12,8 @@
     autojump
     bat
     borgbackup
-    brave
     fzf
     gh
-    git
     htop
     httpie
     nodePackages.jsonlint
@@ -24,12 +22,10 @@
     ncdu
     nmap
     nixfmt-rfc-style
-    pinentry
     rclone
     ripgrep
     speedtest-rs
     vscode
-    wmctrl
     yamllint
     zsh-powerlevel10k
 
@@ -42,21 +38,6 @@
   ];
 
   home.file = {
-    # sway
-    ".config/sway/config.d/50-rules-browser.conf".source = sway/config.d/50-rules-browser.conf;
-    ".config/sway/config.d/50-rules-pavucontrol.conf".source = sway/config.d/50-rules-pavucontrol.conf;
-    ".config/sway/config.d/50-rules-policykit-agent.conf".source = sway/config.d/50-rules-policykit-agent.conf;
-    ".config/sway/config.d/60-bindings-brightness.conf".source = sway/config.d/60-bindings-brightness.conf;
-    ".config/sway/config.d/60-bindings-media.conf".source = sway/config.d/60-bindings-screenshot.conf;
-    ".config/sway/config.d/60-bindings-screenshot.conf".source = sway/config.d/60-bindings-volume.conf;
-    ".config/sway/config.d/60-bindings-volume.conf".source = sway/config.d/65-mode-passthrough.conf;
-    ".config/sway/config.d/65-mode-passthrough.conf".source = sway/config.d/90-bar.conf;
-    ".config/sway/config.d/90-bar.conf".source = sway/config.d/90-swayidle.conf;
-    ".config/sway/config.d/90-swayidle.conf".source = sway/config.d/95-autostart-policykit-agent.conf;
-    ".config/sway/config.d/95-autostart-policykit-agent.conf".source = sway/config.d/95-xdg-desktop-autostart.conf;
-    ".config/sway/config.d/95-xdg-desktop-autostart.conf".source = sway/config.d/95-xdg-user-dirs.conf;
-    ".config/sway/config.d/95-xdg-user-dirs.conf".source = sway/config.d/95-xdg-user-dirs.conf;
-    ".config/sway/config.d/96-1password.conf".source = sway/config.d/96-1password.conf;
     ".gnupg/gpg-agent.conf".source = gnupg/gpg-agent.conf;
     ".gnupg/gpg.conf".source = gnupg/gpg.conf;
 
@@ -67,21 +48,24 @@
     # ".config/kitty/kitty.conf".source = kitty/kitty.conf;
 
     # dunst
-    ".config/dunst/dunstrc".source = dunst/dunstrc;
+    # ".config/dunst/dunstrc".source = dunst/dunstrc;
 
     # waybar
-    ".config/waybar/config.jsonc".source = waybar/config.jsonc;
-    ".config/waybar/style.css".source = waybar/style.css;
+    # ".config/waybar/config.jsonc".source = waybar/config.jsonc;
+    # ".config/waybar/style.css".source = waybar/style.css;
 
     # kanshi
-    ".config/systemd/user/kanshi.service".source = systemd/kanshi.service;
+    # ".config/systemd/user/kanshi.service".source = systemd/kanshi.service;
 
     # foot
-    ".config/foot/foot.ini".source = foot/foot.ini;
+    # ".config/foot/foot.ini".source = foot/foot.ini;
 
     # bin scripts
-    "bin/next-wallpaper.sh".source = bin/next-wallpaper.sh;
-    "bin/previous-wallpaper.sh".source = bin/previous-wallpaper.sh;
+    # "bin/next-wallpaper.sh".source = bin/next-wallpaper.sh;
+    # "bin/previous-wallpaper.sh".source = bin/previous-wallpaper.sh;
+
+    ".local/share/applications/discord.desktop".source = desktop/discord.desktop;
+    ".local/share/applications/todoist.desktop".source = desktop/todoist.desktop;
   };
 
   fonts.fontconfig.enable = true;
@@ -94,11 +78,6 @@
   home.sessionPath = ["$HOME/bin"];
 
   home.changes-report.enable = true;
-
-  home.file = {
-    ".local/share/applications/discord.desktop".source = desktop/discord.desktop;
-    ".local/share/applications/todoist.desktop".source = desktop/todoist.desktop;
-  };
 
   programs.home-manager.enable = true;
 }
